453d77ce98bfd788194a198a61da1ad7b1aba446
gryf <gryf73@gmail.com>
2020-04-12 14:05:56 +0200

    Remove not needed paths from sys module.

--- ebook_converter/main.py
+++ ebook_converter/main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 import sys
 
-# TODO: remove this crap
-sys.resources_location = ''
-sys.extensions_location = '/usr/lib64/calibre/calibre/plugins'
-sys.executables_location = '/usr/bin'
-
 from ebook_converter.ebooks.conversion.cli import main
 
 
