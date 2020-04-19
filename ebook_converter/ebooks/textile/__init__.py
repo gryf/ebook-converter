69d2e536c5cfca12fd0ec259d0f1a87d39296827
gryf <gryf73@gmail.com>
2020-04-19 13:50:42 +0200

    Added txt related modules

---
+++ ebook_converter/ebooks/textile/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from .functions import textile, textile_restricted, Textile
+
+if False:
+    textile, textile_restricted, Textile
+
+__all__ = ['textile', 'textile_restricted']
